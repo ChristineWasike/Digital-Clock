5f61dc05db99675540c39390ecc47ca12885c2d0
Christine Wasike <christinewasike22@gmail.com>
2018-04-05 18:32:27 +0300

    Clock works!

--- js/script.js
+++ js/script.js
@@ -1,18 +1,20 @@
 $(document).ready(function () {
-function displayTime() {
-    var currentTime = new Date();
-    var hours = currentTime.getHours();
-    var minutes = currentTime.getMinutes();
-    var seconds = currentTime.getSeconds();
 
-// This gets a "handle" to the clock div in our HTML
-var clockDiv = document.getElementById('clock');
+    function displayTime() {
+        var currentTime = new Date();
+        var hours = currentTime.getHours();
+        var minutes = currentTime.getMinutes();
+        var seconds = currentTime.getSeconds();
 
-// Then we set the text inside the clock div 
-// to hours, minutes and seconds of the current time
-clockDiv.innerText = hours + ":" + minutes + ":" + seconds;
-}
+        // This gets a "handle" to the clock div in our HTML
+        var clockDiv = document.getElementById('clock');
 
-// This runs the displayTime function the first time
-displayTime();
+        // Then we set the text inside the clock div 
+        // to the hours, minutes, and seconds of the current time
+        clockDiv.innerText = hours + ":" + minutes + ":" + seconds;
+    }
+
+    // This runs the displayTime function the first time
+    displayTime();
+
 });
